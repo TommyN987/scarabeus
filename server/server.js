40eed70e4e82ec60c1d7148e90cede1fea019a09
TommyN987 <tomas.nagy.tn@gmail.com>
2022-05-27 09:20:57 +0200

    set up server, connect to db

---
+++ server/server.js
@@ -0,0 +1,21 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(bodyParser.json({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+mongoose.connect(process.env.CONNECTION_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+  .then(() => app.listen(PORT, () => console.log(`DB connection established, server running on port ${PORT}`)))
+  .catch(err => console.log(err.message));
