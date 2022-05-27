b843ab240aacb69e0ca45ec7eb78da6d2edb9a98
TommyN987 <tomas.nagy.tn@gmail.com>
2022-05-27 09:36:53 +0200

    create user schema and model

---
+++ server/models/user.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String,
+  name: String,
+  role: {
+    type: String,
+    default: 'Submitter'
+  },
+  projects: {
+    type: [mongoose.SchemaTypes.ObjectId],
+    default: []
+  }
+});
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
