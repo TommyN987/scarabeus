0eb11c4b981232632ca2788d51057e63d36b700e
TommyN987 <tomas.nagy.tn@gmail.com>
2022-05-27 10:25:28 +0200

    set up auth context

--- client/src/contexts/AuthContext.tsx
+++ client/src/contexts/AuthContext.tsx
@@ -1,1 +1,34 @@
-export {}
+import { createContext, useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut
+} from 'firebase/auth';
+
+import { auth } from "../firebase";
+import { AuthContextProviderProps, AuthContextType, User } from "../types/types";
+
+export const AuthContext = createContext<AuthContextType | null>(null);
+
+export const createUserInFirebase = (email: string, password: string) => {
+  return createUserWithEmailAndPassword(auth, email, password)
+}
+
+export const loginUser = (email: string, password: string) => {
+  return signInWithEmailAndPassword(auth, email, password)
+}
+
+export const logoutUser = () => {
+  return signOut(auth)
+}
+
+export const AuthContextProvider = ({children}: AuthContextProviderProps) => {
+
+  const [activeUser, setActiveUser] = useState<User | null>(null);
+
+  return (
+    <AuthContext.Provider value={{ activeUser, setActiveUser }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
--- client/src/types/types.ts
+++ client/src/types/types.ts
@@ -1,3 +1,12 @@
+export type AuthContextProviderProps = {
+  children: React.ReactNode
+}
+
+export type AuthContextType = {
+  activeUser: User | null,
+  setActiveUser: React.Dispatch<React.SetStateAction<User | null>>,
+}
+
 export type User = {
   email: string,
   password: string,
