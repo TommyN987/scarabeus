3e2893c6f0e3ccc02d0144521b33b857f6537347
TommyN987 <tomas.nagy.tn@gmail.com>
2022-05-27 09:41:25 +0200

    logic for createUser in db

--- server/controllers/register.js
+++ server/controllers/register.js
@@ -1,3 +1,17 @@
+import User from '../models/user.js'
+
 export const getRegisterPage = (req, res) => {
   res.send('Register Page')
+}
+
+export const createUser = async (req, res) => {
+  const user = req.body;
+  const newUser = new User(user);
+
+  try {
+    await newUser.save();
+    res.status(201).json(newUser);
+  } catch(err) {
+    res.status(409).json({ message: err.message })
+  }
 }
--- server/routes/register-route.js
+++ server/routes/register-route.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { getRegisterPage } from "../controllers/register.js";
+import { createUser, getRegisterPage } from "../controllers/register.js";
 
 const router = express.Router();
 
 router.get('/', getRegisterPage);
+router.post('/', createUser)
 
 export default router;
