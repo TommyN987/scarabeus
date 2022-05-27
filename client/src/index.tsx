e21b047d52fb5cdcc1c1ba7978f2aa491e7046ee
TommyN987 <tomas.nagy.tn@gmail.com>
2022-05-27 11:27:57 +0200

    provide auth context for app

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
+import { AuthContextProvider } from './contexts/AuthContext';
 import App from './App';
 
 const root = ReactDOM.createRoot(
@@ -9,6 +10,8 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <BrowserRouter>
-    <App />
+    <AuthContextProvider>
+      <App />
+    </AuthContextProvider>
   </BrowserRouter>
 );
--- client/src/pages/dashboard/Dashboard.tsx
+++ client/src/pages/dashboard/Dashboard.tsx
@@ -1,6 +1,22 @@
+import { useContext } from 'react';
+
+import { Button } from '@mui/material';
+import { AuthContext } from '../../contexts/AuthContext';
+
 const Dashboard = () => {
+
+  const userContext = useContext(AuthContext);
+  console.log(userContext?.activeUser)
+
   return (
-    <div>Dashboard</div>
+    <>
+      <h1>Dashboard</h1>
+      <div>User: {userContext && userContext.activeUser ? userContext.activeUser.name : 'test'}</div>
+      <Button 
+        variant='contained'
+        >Logout
+      </Button>
+    </>
   )
 }
 export default Dashboard
