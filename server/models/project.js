d5700b4abc15db2cf2291169aa75ddbde611dca5
TommyN987 <tomas.nagy.tn@gmail.com>
2022-06-01 09:50:28 +0200

    corrections to project model

---
+++ server/models/project.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const projectSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  personnel: [String],
+  tickets: {
+    type: [{
+      title: String,
+      description: String,
+      priority: String,
+      submitter: String,
+      solver: {
+        type: String,
+        default: ''
+      },
+      status: {
+        type: String,
+        default: 'Open'
+      },
+      comments: {
+        type: [{
+        commenter: String,
+        message: String,
+        created: {
+          type: Date,
+          default: Date.now
+          }
+        }],
+        default: []
+      },
+    }],
+    default: []
+  }
+}, { timestamps: true });
+
+const Project = mongoose.model('Project', projectSchema);
+
+export default Project;
